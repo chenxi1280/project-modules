f02b667b0cc96e93d189a8ba1141b08190239ec5
PC <chenxi1280@outlook.com>
2020-05-13 22:51:59 +0800

    接口完成

--- mybatiesdemo/src/main/java/com/qf/test/Test.java
+++ mybatiesdemo/src/main/java/com/qf/test/Test.java
@@ -1,7 +1,32 @@
 package com.qf.test;
 
+import com.qf.dao.FcOrderDao;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Test {
     public static void main(String[] args) {
+
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatisConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            FcOrderDao mapper = sqlSession.getMapper(FcOrderDao.class);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
 
 
